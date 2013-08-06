32dde79ca5c559e18864b5c9883f952465e7ac9a
Renato Ribeiro <renatoribeiro@Renatos-MacBook-Air.local>
2013-08-05 23:27:50 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nails::Application.config.secret_token = '670acc3e4199b0f5c4d1f6a5678aa9a0571fc8085965c8966c0116a7c4160fdfba3706f65a9c8b60f5a15441d57dbab670e1cb6f9474575f3b423a5fc0a081cb'
